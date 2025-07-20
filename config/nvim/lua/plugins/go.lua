c737fd56757d8457ae045732e215131403fa56cc
Yang Bo <yangbodotnet@gmail.com>
2025-07-20 10:35:31 +0800

    adding my nvim config

---
+++ config/nvim/lua/plugins/go.lua
@@ -0,0 +1,35 @@
+return {
+  "ray-x/go.nvim",
+  dependencies = { -- optional packages
+    "ray-x/guihua.lua",
+    "neovim/nvim-lspconfig",
+    "nvim-treesitter/nvim-treesitter",
+  },
+  config = function()
+    require("go").setup()
+
+    -- Run gofmt + goimport on save
+    local format_sync_grp = vim.api.nvim_create_augroup("GoImport", {})
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      pattern = "*.go",
+      callback = function()
+        require("go.format").goimport()
+      end,
+      group = format_sync_grp,
+    })
+  end,
+  event = { "CmdlineEnter" },
+  ft = { "go", "gomod" },
+  build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
+
+  keys = {
+    { "<leader>gaj", "<cmd>GoAddTag<cr>", desc = "Add json tags" },
+    { "<leader>gam", "<cmd>GoAddTag mapstructure<cr>", desc = "Add mapstructure tags" },
+    { "<leader>gae", "<cmd>GoAddTag env<cr>", desc = "Add env tags" },
+    { "<leader>gay", "<cmd>GoAddTag yaml<cr>", desc = "Add YAML tags" },
+    { "<leader>gasvr", "<cmd>GoAddTag validate:required<cr>", desc = "Add Swagger validate required tags" },
+    { "<leader>gasvo", "<cmd>GoAddTag validate:optional<cr>", desc = "Add Swagger validate optional tags" },
+
+    { "<leader>gim", "<cmd>GoImplements<cr>", desc = "Find implementions of this method" },
+  },
+}
